022f7d1bc7b28a241afa6d265663c81a4b9440e4
noob-programmer9012 <parth.sclub@gmail.com>
2024-06-23 22:37:49 +0530

    added multer to upload files

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import path from "path";
+import multer from "multer";
+import * as fs from "node:fs";
 
 // Internal imports
 import { errorHandler } from "./middlewares/errorHandler.js";
@@ -19,6 +21,7 @@ const port = process.env.PORT || 8000;
 
 // middlewares
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use("/assets", express.static(path.join(path.resolve(), "assets")));
 app.use((req, res, next) => {
   // CORS doesn't let transfer data between two different server, so it causes CORS errors
@@ -33,8 +36,48 @@ app.use((req, res, next) => {
   next();
 });
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    console.log(req.file);
+    if (file.mimetype === "image/svg+xml") {
+      cb(null, path.join(path.resolve(), "assets", "svg"));
+    } else if (
+      file.mimetype === "image/jpeg" ||
+      file.mimetype === "image/jpg" ||
+      file.mimetype === "image/png"
+    ) {
+      cb(null, path.join(path.resolve(), "assets", "images"));
+    } else if (
+      file.mimetype === "video/mp4" ||
+      file.mimetype === "video/webm" ||
+      file.mimetype === "video/quicktime"
+    ) {
+      cb(null, path.join(path.resolve(), "assets", "images"));
+    }
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
+
 // routes
-app.use("/admin", adminRoutes);
+// app.post("/upload", upload.single("file"), (req, res, next) => {
+//   // const { buffer, originalname } = req.file;
+//   // const writeStream = fs.createWriteStream(originalname, { flags: "a" });
+//   // writeStream.write(buffer);
+//   // writeStream.end();
+//   // writeStream.on("finish", () => {
+//   //   res.status(200).send("File received successfully.");
+//   // });
+//   // // Event listener for any errors during the write operation
+//   // writeStream.on("error", (err) => {
+//   //   console.error(err);
+//   //   res.status(500).send("Internal Server Error");
+//   // });
+// });
+app.use("/admin", upload.single("file"), adminRoutes);
 app.use("/auth", authRoutes);
 app.use("/dashboard", isAuth, (req, res, next) => {
   if (req.isUser) {
