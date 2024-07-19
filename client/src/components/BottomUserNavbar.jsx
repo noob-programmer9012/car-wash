549d74c9c723961b10bddb16d73662809fb6d976
noob-programmer9012 <parth.sclub@gmail.com>
2024-07-19 22:21:32 +0530

    bottom navbar in use page

--- client/src/components/BottomUserNavbar.jsx
+++ client/src/components/BottomUserNavbar.jsx
@@ -1,11 +1,45 @@
+import HomeIcon from "@mui/icons-material/Home";
+import HistoryIcon from "@mui/icons-material/History";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+
+import { Typography } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
 export default function BottomUserNavbar() {
   return (
     <div className="bottom-nav">
-      <ul>
-        <li>One</li>
-        <li>Two</li>
-        <li>Three</li>
-      </ul>
+      <div className="navigation">
+        <div className="nav">
+          <NavLink
+            to="/"
+            className={({ isActive, isPending }) =>
+              isPending ? "pending" : isActive ? "active" : ""
+            }
+            end
+          >
+            <div className="navlink">
+              <HomeIcon className="bottom-icons" />
+              <Typography variant="p">Home</Typography>
+            </div>
+          </NavLink>
+        </div>
+        <div className="nav">
+          <NavLink to="/bookings">
+            <div className="navlink">
+              <HistoryIcon className="bottom-icons" />
+              <Typography variant="p">Bookings</Typography>
+            </div>
+          </NavLink>
+        </div>
+        <div className="nav">
+          <NavLink to="/profile">
+            <div className="navlink">
+              <AccountCircleIcon className="bottom-icons" />
+              <Typography variant="p">Profile</Typography>
+            </div>
+          </NavLink>
+        </div>
+      </div>
     </div>
   );
 }
